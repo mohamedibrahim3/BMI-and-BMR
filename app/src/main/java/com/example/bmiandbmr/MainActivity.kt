b7d3e34fd56db574d0c6faa50b80eafe4160fe90
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2023-02-16 13:51:54 +0200

    First Commit

---
+++ app/src/main/java/com/example/bmiandbmr/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.bmiandbmr
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.InputType
+import android.view.Gravity
+import com.example.bmiandbmr.databinding.ActivityMainBinding
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+
+
+class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding?.ageEditText?.inputType = InputType.TYPE_CLASS_NUMBER
+        binding?.heightEditText?.inputType = InputType.TYPE_CLASS_NUMBER
+        binding?.weightEditText?.inputType = InputType.TYPE_CLASS_NUMBER
+        binding?.ageEditText?.hint = getString(R.string.YourAge)
+        binding?.heightEditText?.hint = getString(R.string.CM)
+        binding?.weightEditText?.hint = getString(R.string.KM)
+        binding?.ageEditText?.let {
+            setHint(it, R.string.YourAge)
+        }
+        binding?.heightEditText?.let {
+            setHint(it, R.string.CM)
+        }
+        binding?.weightEditText?.let {
+            setHint(it, R.string.KM)
+        }
+        binding?.NextBtntInBMI?.setOnClickListener {
+            if(binding?.ageEditText?.text?.isEmpty() == true) {
+                binding?.Star1?.setTextColor(Color.parseColor("#EB1D36"))
+            }
+            else if (binding?.ageEditText?.text?.isEmpty() != true)
+                binding?.Star1?.setTextColor(Color.parseColor("#868484"))
+            if(binding?.heightEditText?.text?.isEmpty() == true) {
+                binding?.Star2?.setTextColor(Color.parseColor("#EB1D36"))
+            }
+            else if (binding?.heightEditText?.text?.isEmpty() != true)
+                binding?.Star2?.setTextColor(Color.parseColor("#868484"))
+            if(binding?.weightEditText?.text?.isEmpty() == true) {
+                binding?.Star3?.setTextColor(Color.parseColor("#EB1D36"))
+            }
+            else if (binding?.weightEditText?.text?.isEmpty() != true)
+                binding?.Star3?.setTextColor(Color.parseColor("#868484"))
+        }
+        //binding?.Star1?.setTextColor(Color.parseColor("#EB1D36"))
+        setContentView(binding?.root)
+    }
+    private fun setHint(view: TextInputEditText, string: Int) {
+        view.setOnFocusChangeListener { v, hasFocus ->
+            if (hasFocus) {
+                view.hint = ""
+                (v as TextInputEditText).gravity = Gravity.RIGHT
+            } else {
+                if (view.text?.isEmpty() == true) {
+                    view.hint = getString(string)
+                    (v as TextInputEditText).gravity = Gravity.RIGHT
+                }
+            }
+        }
+    }
+
+}
