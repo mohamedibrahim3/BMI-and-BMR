98998b3d668c6e94c2b7ed2e205b246c9e02029b
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2023-02-19 15:46:03 +0200

    add 2 termometers

--- app/src/main/java/com/example/bmiandbmr/Verify_data_MainActivity.kt
+++ app/src/main/java/com/example/bmiandbmr/Verify_data_MainActivity.kt
@@ -7,14 +7,16 @@ import android.os.Bundle
 import com.example.bmiandbmr.databinding.ActivityVerifyDataMainBinding
 
 class Verify_data_MainActivity : AppCompatActivity() {
-    var curProgress = 130
+    var curProgressKm = 110
+    var curProgressCm = 160
     private lateinit var binding: ActivityVerifyDataMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityVerifyDataMainBinding.inflate(layoutInflater)
 
         binding.btnEdit.setOnClickListener {
-            binding.progressBarKmr.progress = curProgress
+            binding.progressBarKmr.progress = curProgressKm
+            binding.progressBarCr.progress = curProgressCm
         }
         binding.btnCal.setOnClickListener {
             Intent(this,ResultBmrBmi::class.java).also {
