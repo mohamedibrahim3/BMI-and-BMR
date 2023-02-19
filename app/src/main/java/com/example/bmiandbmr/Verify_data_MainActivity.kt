a4140fd5aa06e4c673fa4de63cc2dfb14dcde5c2
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2023-02-19 22:47:30 +0200

    add Slider

--- app/src/main/java/com/example/bmiandbmr/Verify_data_MainActivity.kt
+++ app/src/main/java/com/example/bmiandbmr/Verify_data_MainActivity.kt
@@ -7,16 +7,18 @@ import android.os.Bundle
 import com.example.bmiandbmr.databinding.ActivityVerifyDataMainBinding
 
 class Verify_data_MainActivity : AppCompatActivity() {
-    var curProgressKm = 90
-    var curProgressCm = 186
+    var curProgressKm = 40
+    var curProgressCm = 130
     private lateinit var binding: ActivityVerifyDataMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityVerifyDataMainBinding.inflate(layoutInflater)
-
         binding.btnEdit.setOnClickListener {
+            val layoutParams = binding.imgWomanId.layoutParams
+            layoutParams.height = 160
+            binding.imgWomanId.layoutParams = layoutParams
             binding.progressBarKmr.progress = curProgressKm
-            binding.progressBarCr.progress = curProgressCm
+            binding.progressBarCr.progress = curProgressCm + 2
         }
         binding.btnCal.setOnClickListener {
             Intent(this,ResultBmrBmi::class.java).also {
