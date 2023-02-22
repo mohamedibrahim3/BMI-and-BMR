900a454571544bb676f139093e37bf3a166f10f6
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2023-02-22 20:09:35 +0200

    Finish BMI

--- app/src/main/java/com/example/bmiandbmr/BmiFragment.kt
+++ app/src/main/java/com/example/bmiandbmr/BmiFragment.kt
@@ -1,12 +1,15 @@
 package com.example.bmiandbmr
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintSet
 import com.example.bmiandbmr.databinding.FragmentBmi2Binding
+import kotlin.math.roundToInt
 
 
 class BmiFragment : Fragment() {
@@ -42,7 +45,45 @@ class BmiFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         _binding = FragmentBmi2Binding.inflate(inflater, container, false)
-        binding.txtResIdBmi.text = "وزن طبيعي$bmi"
+        val bmi1 = bmi!!.roundToInt()
+        binding.lineBId.visibility = View.INVISIBLE
+        binding.lineGId.visibility = View.INVISIBLE
+        binding.lineOId.visibility = View.INVISIBLE
+        binding.lineRId.visibility = View.INVISIBLE
+        if(gender == "أنثى"){
+            binding.womanResIdBmi.visibility = View.VISIBLE
+            binding.manResIdBmi.visibility = View.INVISIBLE
+        }
+
+        if(bmi!! < 18.5){
+            binding.txtResIdBmi.text = "نقص الوزن$bmi1 "
+            binding.txtResIdBmi.setTextColor(Color.parseColor("#1976D2"))
+            binding.lineBId.visibility = View.VISIBLE
+            binding.lineBId.translationX = (bmi!! * 11).toFloat()
+
+        }
+        else if (bmi!! >= 18.5 && bmi!! < 25){
+            binding.txtResIdBmi.text = "وزن طبيعى$bmi1 "
+            binding.txtResIdBmi.setTextColor(Color.parseColor("#388E3C"))
+            binding.lineGId.visibility = View.VISIBLE
+            binding.lineGId.translationX = (bmi!! * 2.5).toFloat()
+        }
+        else if (bmi!! >= 25 && bmi!! < 30){
+            binding.txtResIdBmi.text = "وزن زائد$bmi1 "
+            binding.txtResIdBmi.setTextColor(Color.parseColor("#FF8000"))
+            binding.lineOId.visibility = View.VISIBLE
+            binding.lineOId.translationX = (bmi!!-20).toFloat()
+        }
+        else{
+            binding.txtResIdBmi.text = "سمنة زائدة$bmi1 "
+            binding.txtResIdBmi.setTextColor(Color.parseColor("#FF5252"))
+            binding.lineRId.visibility = View.VISIBLE
+            if(bmi!!>=40)
+                binding.lineRId.translationX = (40*6 + 15).toFloat()
+            else
+            binding.lineRId.translationX = (bmi!!*3).toFloat()
+        }
+
         return binding.root
     }
     override fun onDestroyView() {
